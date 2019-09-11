a1002298fdba014dfadf8f4fd8229e901fae507f
suozhaoyu <suozhaoyu0530@163.com>
2019-09-11 19:00:08 +0800

    第三题

--- src/t1to50/t3_longest_substring.rs
+++ src/t1to50/t3_longest_substring.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+use std::cmp;
+
 /// 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
 /// 
 /// example 1:
@@ -22,6 +25,115 @@
 /// 并用一个变量front记录下上次重复的位置，在front之前记录的字符位置不作数需要重新记录
 /// 用来排除asdfghdqwea这种情况,d已经重复，所以a-a不作数
 pub fn length_of_longest_substring(s: String) -> i32 {
-    let front: usize = 0;
+    let mut front = 0;
+    let mut max_length = 0;
 
+    let mut map = HashMap::new();
+
+    let mut i = 0;
+    for c in s.chars() {
+        if map.contains_key(&c) {
+            let c_index = map.get_mut(&c).unwrap();
+
+            if *c_index >= front {
+                max_length = cmp::max(max_length, i-front);
+
+                front = *c_index+1;
+                max_length = cmp::max(max_length, i-*c_index);
+                *c_index = i;
+                i = i+1;
+                continue;
+            }
+
+            *c_index = i;
+        }
+
+        map.insert(c, i);
+        i = i+1;
+    }
+
+    max_length = cmp::max(max_length, i-front);
+
+    max_length
+}
+
+#[test]
+fn example1() {
+    let s = String::from("abcabcbb");
+    let max_length_expected = 3;
+    let max_length = length_of_longest_substring(s);
+
+    assert_eq!(max_length, max_length_expected);
+}
+
+#[test]
+fn example2() {
+    let s = String::from("bbbbb");
+    let max_length_expected = 1;
+    let max_length = length_of_longest_substring(s);
+
+    assert_eq!(max_length, max_length_expected);
+}
+
+#[test]
+fn example3() {
+    let s = String::from("pwwkew");
+    let max_length_expected = 3;
+    let max_length = length_of_longest_substring(s);
+
+    assert_eq!(max_length, max_length_expected);
+}
+
+#[test]
+fn example4() {
+    let s = String::from("cdd");
+    let max_length_expected = 2;
+    let max_length = length_of_longest_substring(s);
+
+    assert_eq!(max_length, max_length_expected);
+}
+
+#[test]
+fn example5() {
+    let s = String::from("dvdf");
+    let max_length_expected = 3;
+    let max_length = length_of_longest_substring(s);
+
+    assert_eq!(max_length, max_length_expected);
+}
+
+#[test]
+fn example6() {
+    let s = String::from(" ");
+    let max_length_expected = 1;
+    let max_length = length_of_longest_substring(s);
+
+    assert_eq!(max_length, max_length_expected);
+}
+
+#[test]
+fn example7() {
+    let s = String::from("abba");
+    let max_length_expected = 2;
+    let max_length = length_of_longest_substring(s);
+
+    assert_eq!(max_length, max_length_expected);
+}
+
+#[test]
+fn example8() {
+    let s = String::from("ohomm");
+    let max_length_expected = 3;
+    let max_length = length_of_longest_substring(s);
+
+    assert_eq!(max_length, max_length_expected);
+}
+
+#[test]
+fn example9() {
+    let s = String::from("abcb");
+    let max_length_expected = 3;
+    let max_length = length_of_longest_substring(s);
+
+    assert_eq!(max_length, max_length_expected);
 }
