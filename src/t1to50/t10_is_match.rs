ff72d03e9ca35e55eb4c9fca42d12f1d4e86856f
suozhaoyu <suozhaoyu0530@163.com>
2019-09-24 18:36:56 +0800

    第十题 做错了 重做

--- src/t1to50/t10_is_match.rs
+++ src/t1to50/t10_is_match.rs
@@ -47,119 +47,7 @@
 /// 如 a*a*a*=a*  .*a*b*c*=.*  a*b*c*.*=.*
 /// 然后分组往后推移字符的检测位置
 fn is_match(s: String, p: String) -> bool {
-    let mut p_v = Vec::<Vec<char>>::new();
-
-    for c in p.chars() {
-        if c == '*' {
-            if !p_v.is_empty() {
-                let c_front_v = p_v.last_mut().unwrap();
-                let c_f = c_front_v[0];
-                if (c_front_v.len() == 1 && c_f == '*') || c_front_v.len() == 2 {
-                    continue;
-                } else {
-                    c_front_v.push(c);
-                }
-
-                if p_v.len() == 1 {
-                    continue;
-                }
-
-                if c_f == '.' {
-                    for i in (0..p_v.len()-1).rev() {
-                        if p_v.get(i).unwrap().len() == 2 {
-                            p_v.remove(i);
-                        } else {
-                            break;
-                        }
-                    }
-                } else {
-                    let c_valid = p_v.get(p_v.len()-2).unwrap();
-                    let c_v = c_valid[0];
-                    if c_valid.len() == 2 && (c_v == '.' || c_f == c_v) {
-                        p_v.pop();
-                    }
-                }
-            } else {
-                let c_v = vec![c];
-                p_v.push(c_v);
-            }
-        } else {
-            let c_v = vec![c];
-            p_v.push(c_v);
-        }
-    }
-
-    let mut c_ignore: Option<char> = None;
-    let mut pv_index = 0_usize;
-    let mut is_fuzzy = false;
-    let mut fuzzy_index = 0_usize;
-    let mut fuzzy_jud = false;
-    for c in s.chars() {
-        if c_ignore.is_some() && c_ignore.unwrap() == c && !is_fuzzy {
-            continue;
-        }
-
-        if pv_index == p_v.len() {
-            return false;
-        }
-
-        while pv_index < p_v.len() {
-            let cs_valid = p_v.get(pv_index).unwrap();
-            let c_f = cs_valid.first().unwrap();
-
-            println!("c_f: {}    c: {}", c_f, c);
-
-            if c_f == &'*' {
-                break;
-            } else if c_f == &'.' {
-                if is_fuzzy && !fuzzy_jud {
-                    return false;
-                }
-                if cs_valid.len() == 2 {
-                    is_fuzzy = true;
-                    fuzzy_index = pv_index + 1;
-                }
-                if pv_index + 1 == p_v.len() && cs_valid.len() == 2 {
-                    return true;
-                }
-                pv_index = pv_index + 1;
-                break;
-            } else if c_f == &c {
-                if is_fuzzy {
-                    fuzzy_jud = true;
-                }
-                pv_index = pv_index + 1;
-                if cs_valid.len() == 2 {
-                    c_ignore = Some(c);
-                } else {
-                    c_ignore = None;
-                }
-                break;
-            } else {
-                if is_fuzzy {
-                    let c_fuzzy = p_v.get(fuzzy_index).unwrap().first().unwrap();
-                    if c_fuzzy == &c {
-                        pv_index = fuzzy_index + 1;
-                    } else {
-                        pv_index = fuzzy_index;
-                    }
-                    fuzzy_jud = true;
-                    break;
-                } else {
-                    return false;
-                }
-            }
-        }
-    }
-
-    for i in pv_index..p_v.len() {
-        let cs_valid = p_v.get(i).unwrap();
-        if cs_valid.len() < 2 {
-            return false;
-        }
-    }
-
-    true
+    
 }
 
 #[test]
@@ -190,6 +78,46 @@ fn example3() {
 fn example4() {
     let s = String::from("aab");
     let p = String::from("c*a*b");
+
+    assert!(is_match(s, p));
+}
+
+#[test]
+fn example5() {
+    let s = String::from("mississippi");
+    let p = String::from("mis*is*p*.");
+
+    assert!(!is_match(s, p));
+}
+
+#[test]
+fn example6() {
+    let s = String::from("aaa");
+    let p = String::from("ab*a*c*a");
+
+    assert!(is_match(s, p));
+}
+
+#[test]
+fn example7() {
+    let s = String::from("mississippi");
+    let p = String::from("mis*is*ip*.");
+
+    assert!(is_match(s, p));
+}
+
+#[test]
+fn example8() {
+    let s = String::from("ab");
+    let p = String::from(".*ab");
+
+    assert!(is_match(s, p));
+}
+
+#[test]
+fn example9() {
+    let s = String::from("ab");
+    let p = String::from(".*..");
 
     assert!(is_match(s, p));
 }
